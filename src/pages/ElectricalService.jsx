0d390d2cbd89968a3069b32833ae4d1706a3a1bc
Dilipsinh Jadeja <dilipsinh.j@unomok.com>
2021-10-18 11:40:27 +0530

    Home-4 is completed

--- src/pages/ElectricalService.jsx
+++ src/pages/ElectricalService.jsx
@@ -7,6 +7,7 @@ import electricalServiceBadge from "../assests/images/electricalServiceBadge.svg
 import pinkImg from "../assests/images/pinkImg.svg";
 import orangeImage from "../assests/images/orangeImage.svg";
 import blackImg from "../assests/images/blackImg.svg";
+import { withTheme } from "styled-components";
 
 const ElectricalService = () => {
   return (
@@ -21,12 +22,13 @@ const ElectricalService = () => {
             marginTop: 20,
             marginRight: 20,
             borderRadius: 20,
+            paddingBottom: 20,
           }}
         >
           <Topbar back={back} />
           <Row className="mt-3">
             <Col style={{ padding: 15 }}>
-              <Row>
+              <Row style={{ paddingLeft: "10px" }}>
                 <Col className="mb-3 me-2">
                   <div
                     className="badgeActive"
@@ -67,7 +69,7 @@ const ElectricalService = () => {
                     }}
                   >
                     <Row>
-                      <Col className="p-0">
+                      <Col style={{ padding: "0px 10px" }}>
                         <Image
                           src={electricalServiceBadge}
                           style={{ width: 126, height: 126 }}
@@ -80,9 +82,28 @@ const ElectricalService = () => {
                           alignItems: "center",
                           textAlign: "left",
                           fontSize: "20px",
+                          position: "relative",
                         }}
                       >
-                        <span>Electrical Services</span>
+                        <span style={{ fontSize: "1rem", fontWeight: "bold" }}>
+                          Electrical Services
+                        </span>
+                        <div
+                          style={{
+                            position: "absolute",
+                            bottom: -31,
+                            background: "white",
+                            color: "#484848",
+                            display: "flex",
+                            flexDirection: "column",
+                            alignItems: "center",
+                            right: 6,
+                            borderRadius: "0px 0px 10px 0px",
+                          }}
+                        >
+                          <span style={{ fontSize: 10 }}>Badge Collected</span>
+                          <span style={{ fontSize: 14 }}>21-03-2021</span>
+                        </div>
                       </Col>
                     </Row>
                   </Card>
@@ -118,7 +139,57 @@ const ElectricalService = () => {
                 borderRadius: 20,
                 marginRight: 20,
               }}
-            ></Col>
+            >
+              <p
+                className="text-start mt-3 mb-1 p-0"
+                style={{ marginLeft: 10, fontSize: "0.9rem", color: "#DC7832" }}
+              >
+                Badge for Electrical Services
+              </p>
+              <h4 className="text-start mt-0 p-0" style={{ marginLeft: 10 }}>
+                <u>Electrical Service</u>
+              </h4>
+              <p
+                className="text-start mt-4 p-0"
+                style={{ marginLeft: 10, fontSize: 19 }}
+              >
+                Steps to be followed
+              </p>
+
+              <Row style={{ padding: 10 }}>
+                <Col style={{ margin: "auto" }} xs={2}>
+                  <span className="round">1</span>
+                </Col>
+                <Col style={{ background: "#f8f8f8" }}>
+                  <p>Step 1</p>
+                  <p style={{ color: "grey" }}>
+                    Lorem ipsum dolor sit amet, consetetur sadipscing
+                  </p>
+                </Col>
+              </Row>
+              <Row style={{ padding: 10 }}>
+                <Col style={{ margin: "auto" }} xs={2}>
+                  <span className="round">2</span>
+                </Col>
+                <Col style={{ background: "#f8f8f8" }}>
+                  <p>Step 2</p>
+                  <p style={{ color: "grey" }}>
+                    Lorem ipsum dolor sit amet, consetetur sadipscing
+                  </p>
+                </Col>
+              </Row>
+              <Row style={{ padding: 10 }}>
+                <Col style={{ margin: "auto" }} xs={2}>
+                  <span className="round">3</span>
+                </Col>
+                <Col style={{ background: "#f8f8f8" }}>
+                  <p>Step 3</p>
+                  <p style={{ color: "grey" }}>
+                    Lorem ipsum dolor sit amet, consetetur sadipscing
+                  </p>
+                </Col>
+              </Row>
+            </Col>
           </Row>
         </div>
       </div>
